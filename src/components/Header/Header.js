1c3029ccadf66d839b5c1342feaba8639f071585
Dima Pokidchenko <dimapokidchenko12@gmail.com>
2026-04-03 22:25:56 +0200

    Made half of the media, started making popover

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,7 +17,7 @@ const createHeaderLinks = (currentPath) => {
 
 export const Header = (currentPath) => {
 
-  return `
+  return /*html*/ `
     <header class="header">
      <div class="header_container">
      <img class="logo_header" src="/Icon/logo.svg" alt="Спец-Транс">
@@ -27,7 +27,18 @@ export const Header = (currentPath) => {
      <button class="btn_header">
         <span class="btn_header_title">Заказать звонок</span>
      </button>
+     <button class="burger" popovertarget="burger_menu" type="button">
+     <span class="burger_icon">
+     <img src="/Icon/burgerMenu.svg" alt="Burger Menu"></span></button>
      </div>
+     <div popover id="burger_menu" class="burger_menu">
+     <div class="burger_window">
+     <button class="burger_close" popovertarget="burger_menu" popovertargetaction="hide" type="button"><img class="burger_Close" src="/Icon/burgerclose.svg" alt="Burger Close" /></button></div>
+     <nav class="burger_nav">
+      ${createHeaderLinks(currentPath)}
+    </nav>
+    <button class="burger_btn">Заказать звонок</button>
+    <p class="confidentiality_burger">Политика конфиденциальности © спец-транс, 2026</p>
      </header>
     `;
 };
